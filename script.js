ed7318857c95a1492bfb897b862ea56f86b489aa
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-07-10 07:59:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 //your code here!
 
+
+const infiList = document.getElementById('infi-list');
+
+function checkScroll() {
+  const totalHeight = infiList.scrollHeight;
+  const currentPosition = infiList.scrollTop + infiList.clientHeight;
+
+  console.log('Total Height:', totalHeight);
+  console.log('Current Position:', currentPosition);
+
+  if (totalHeight <= currentPosition + 5) {
+    loadItems(2);
+  }
+}
+
+let count = 1;
+
+function loadItems(num){
+  for(let i=0;i<num;i++){
+    const item = document.createElement('li');
+    item.textContent = `Item ${count++}`;
+    infiList.appendChild(item);
+  }
+}
+
+loadItems(10);
+
+infiList.addEventListener('scroll', checkScroll);
